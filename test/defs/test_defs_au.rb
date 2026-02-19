c98d01857f4ec254d5b8c6337c80029dcd4e0b6f
Mikko Tan <mikko.tan@shiftcare.com>
2026-02-19 11:24:22 +0800

    Add ANZAC Day 2026 observed holiday for NSW (April 27)
    ANZAC Day falls on Saturday April 25, 2026. NSW has announced
    Monday April 27 as an additional public holiday for 2026 only.

--- test/defs/test_defs_au.rb
+++ test/defs/test_defs_au.rb
@@ -94,6 +94,9 @@ assert_equal "ANZAC Day", Date.civil(2015, 4, 25).holidays(:au_wa)[0][:name]
 assert_equal [], Date.civil(2015, 4, 27).holidays(:au_qld, :observed)
 assert_equal "ANZAC Day", Date.civil(2015, 4, 27).holidays(:au_wa, :observed)[0][:name]
 
+# ANZAC Day 2026 - NSW observes on Monday April 27 (one-off, 2026 only)
+assert_equal "ANZAC Day", Date.civil(2026, 4, 27).holidays(:au_nsw, :observed)[0][:name]
+
 # BOXING DAY - QLD observes weekend and monday
 assert_equal "Boxing Day", Date.civil(2015, 12, 26).holidays(:au_qld)[0][:name]
 assert_equal "Boxing Day", Date.civil(2015, 12, 28).holidays(:au_qld, :observed)[0][:name]
